ef1697733d7f7a5b050a25be736a2abe19453112
shay slonim <shayslonim@gmail.com>
2019-03-17 16:17:40 +0200

    Bug fixes, used the Invoke method when the Calculate method is called

--- AdvancedProgramming2_ex1/ComposedMission.cs
+++ AdvancedProgramming2_ex1/ComposedMission.cs
@@ -8,27 +8,36 @@ namespace Excercise_1
 {
     public class ComposedMission : IMission
     {
+        //This is the type of the composed mission
+
+        private const string MISSION_TYPE_NAME = "Composed";
+
         private ICollection<Func<double, double>> funcsHolder;
+
+        public event EventHandler<double> OnCalculate;
+
         public string Name { get; private set; }
-        public string Type { get; private set; }
+        public string Type
+        {
+            get { return ComposedMission.MISSION_TYPE_NAME; }
+        }
 
         public ComposedMission(string name)
         {
+            this.funcsHolder = new List<Func<double, double>>();
             this.Name = name;
         }
-        public ComposedMission Add(Func<double,double> function)
+        public ComposedMission Add(Func<double, double> function)
         {
             this.funcsHolder.Add(function);
             return this;
         }
 
-        public event EventHandler<double> OnCalculate;
-
         public double Calculate(double value)
         {
             //if there are functions in the funcsHodler
             if (funcsHolder.Count() > 0)
-            {   
+            {
                 //calculate the value
                 double result = 0;
                 foreach (var function in funcsHolder)
@@ -36,6 +45,7 @@ namespace Excercise_1
                     result = function(value);
                     value = result;
                 }
+                this.OnCalculate?.Invoke(this, result);
                 return result;
             }
             else
